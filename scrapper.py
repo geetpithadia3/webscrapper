6951bd5d5b41f60dbec6d09d917e7f556d115980
geetpithadia3 <gpithadia@gmail.com>
2020-02-16 13:52:00 -0500

    exception handling

--- scrapper.py
+++ scrapper.py
@@ -8,10 +8,12 @@ driver.maximize_window()
 
 data = pd.DataFrame(columns=["Title","Location","Company","Salary","Date","Description"])
 
-for page in range(0,10,10):
-    driver.get('https://www.indeed.ca/jobs?q=developer&start='+str(page))
+for page in range(0,100,10):
     
+    driver.get('https://www.indeed.ca/jobs?q=developer&start='+str(page))
+    driver.implicitly_wait(10)
     for job in driver.find_elements_by_class_name('result'):
+        
         soup = BeautifulSoup(job.get_attribute('innerHTML'),'html.parser')
         
         title = soup.findAll("a",{"class": "jobtitle turnstileLink"})
@@ -21,25 +23,25 @@ for page in range(0,10,10):
           title = "NaN" 
           
         location = soup.find(class_="location")
-        if location != NULL:
+        if location != None:
           location = location.text.replace("\n","").strip()
         else:
           location = "NaN" 
           
         company = soup.find(class_="company")
-        if company != NULL:
+        if company != None:
           company = company.text.replace("\n","").strip()
         else:
           company = "NaN" 
           
         salary = soup.find(class_="salary")
-        if salary != NULL:
+        if salary != None:
           salary = salary.text.replace("\n","").strip()
         else:
           salary = "NaN" 
         
         date = soup.find("span",class_="date")
-        if date != NULL:
+        if date != None:
           date = date.text.replace("\n","").strip()
         else:
           date = "NaN" 
@@ -53,15 +55,13 @@ for page in range(0,10,10):
             close_button.click()
             summary.click()
             
+        try:
+            job_desc = driver.find_element_by_id('vjs-desc')
+            job1=job_desc.text.replace("\n","")
+        except:
+            job1=""
         
-        job_desc = driver.find_element_by_id('vjs-desc')
-        job1=job_desc.text.replace("\n","")
-        print(job1)
-       
-    
         data = data.append({'Title':title,'Location':location,"Company":company,"Salary":salary,"Date":date,"Description":job1},ignore_index=True)
         
         
-        
-
 data.to_csv("indeed.csv",index=False)	
